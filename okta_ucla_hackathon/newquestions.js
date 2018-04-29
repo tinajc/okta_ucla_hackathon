fd4192ac64dc6955dad5d79ea0fa328240cb7566
Juncai Luo <juncailuo@Juncais-MacBook-Pro.local>
2018-04-29 14:12:44 -0700

    add hackathon files

---
+++ okta_ucla_hackathon/newquestions.js
@@ -0,0 +1,63 @@
+module.exports = {
+  /**
+   * When editing your questions pay attention to your punctuation.
+   * Make sure you use question marks.
+   * Make sure the first answer is the correct one.
+   * Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+   */
+
+  QUESTIONS_EN_US: [
+    {
+      '<audio src="https://s3.amazonaws.com/songforalexaskill/baby.mp3" /> ': 'baby',
+      "answers": [
+        'baby',
+        'paparazi',
+        'boyfriend',
+        'never say never',
+      ],
+      "genre" : 'pop'
+    },
+    {
+      '<audio src="https://s3.amazonaws.com/songforalexaskill/Hotline_Bling.mp3" /> ': 'Hotline bling',
+      "answers": [
+        'Hotline Bling',
+        'one dance',
+        'fake love',
+        'watch me',
+      ],
+      "genre" : 'Hip Hop'
+    },
+    {
+      '<audio src="https://s3.amazonaws.com/songforalexaskill/shake_it_off.mp3"/>': 'shake it off',
+      "answers": [
+        'shake if off',
+        'gorgeous',
+        'you belong with me',
+        'bang bang',
+      ],
+      "genre" : 'pop'
+      
+    },
+    {
+      '<audio src="https://s3.amazonaws.com/songforalexaskill/fly_me_to_the_moon.mp3"/>': 'fly me to the moon',
+      "answers": [
+        'fly me to the moon',
+        'let me see',
+        'without me',
+        'mirror',
+      ],
+      "genre" : 'jazz'
+    },
+    {
+      '<audio src="https://s3.amazonaws.com/songforalexaskill/wonderful_world.mp3"/>': 'what a wonderful world',
+      "answers": [
+        'what a wonderful world',
+        'wonderful life',
+        'wonderful tonight',
+        'wonderful world',
+      ],
+      "genre" : 'jazz'
+    },
+    
+  ],
+};
---
+++ okta_ucla_hackathon/questions.js
@@ -0,0 +1,53 @@
+module.exports = {
+  /**
+   * When editing your questions pay attention to your punctuation.
+   * Make sure you use question marks.
+   * Make sure the first answer is the correct one.
+   * Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+   */
+
+  QUESTIONS_EN_US: [
+    {
+      'what is my name?': [
+        'Brian Lee',
+        'Tina',
+        'Gina',
+        'Maymay',
+      ],
+    },
+    {
+      'who am I': [
+        'god',
+        'douche',
+        'harry',
+        'potter',
+      ],
+    },
+    {
+      'who are you?': [
+        'loser',
+        'x',
+        'b',
+        'c',
+      ],
+    },
+    {
+      'is this really working?': [
+        'yes',
+        'df',
+        'ff',
+        'gg',
+      ],
+    },
+    {
+      'gg': [
+        'gg',
+        '1',
+        '19 miles per hour',
+        '14 miles per hour',
+        '52 miles per hour',
+        '41 miles per hour',
+      ],
+    },
+  ],
+};
